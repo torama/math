0b249f879d238da0e6ef0b5c33594e7a06b284d4
torama <torben.amann@googlemail.com>
2014-01-20 16:35:58 +0100

    Initialimport

---
+++ src/main/java/com/torama/HomePage.java
@@ -0,0 +1,25 @@
+package com.torama;
+
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.request.resource.CssResourceReference;
+import org.apache.wicket.request.resource.JavaScriptResourceReference;
+import org.apache.wicket.markup.head.CssReferenceHeaderItem;
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.JavaScriptReferenceHeaderItem;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.WebPage;
+
+public class HomePage extends WebPage {
+	private static final long serialVersionUID = 1L;
+
+	public HomePage(final PageParameters parameters) {
+		super(parameters);
+
+		add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
+    }
+	
+	@Override public void renderHead(IHeaderResponse response) {
+		response.render(JavaScriptReferenceHeaderItem.forReference(new JavaScriptResourceReference(HomePage.class, "resources/js/HomePage.js")));
+		response.render(CssReferenceHeaderItem.forReference(new CssResourceReference(HomePage.class, "resources/css/HomePage.css")));
+	}
+}
